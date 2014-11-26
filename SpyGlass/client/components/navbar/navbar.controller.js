af607567a3e2876445ab41923f672bfd98b93316
amir sh <amir.shrir@gmail.com>
2014-11-26 16:07:47 +0200

    touchups

--- SpyGlass/client/components/navbar/navbar.controller.js
+++ SpyGlass/client/components/navbar/navbar.controller.js
@@ -3,7 +3,7 @@
 angular.module('spyGlassApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
     $scope.menu = [{
-      'title': 'התרעות',
+      'title': 'התראות',
       'link': '/'
     },
       {
